d121e29e80ee70b9880e89afa232452359840ff5
dstearle <dallas.st.earle@gmail.com>
2019-08-30 02:24:39 -0700

    Gave grade inputs dropdowns and letter grades

--- src/components/StudentInput.vue
+++ src/components/StudentInput.vue
@@ -2,21 +2,113 @@
 
   <div id="container">
 
-      <div class="row">
+    <form action="" class="row">
 
-          <div class="col-2">Name</div>
+        <!-- Name Input -->
+        <div class="form-group col-2">
+            
+            <label for="name" class="float-left font-weight-bold">Student Name</label>
 
-          <div class="col-2">Math</div>
+            <input 
+                type="text" 
+                id="name" 
+                placeholder="Enter Name" 
+                class="form-control"
+            >
 
-          <div class="col-2">History</div>
+        </div>
 
-          <div class="col-2">Science</div>
+        <!-- Math Grade Input -->
+        <div class="form-group col-2">
+            
+            <label for="math" class="float-left font-weight-bold">Math</label>
+            
+            <select id="math" class="form-control">
 
-          <div class="col-2">English</div>
+                <option selected>-</option>
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+                <option>D</option>
+                <option>F</option>
+                
+            </select>
 
-          <div class="col-2">Submit</div>
+        </div>
 
-      </div>
+        <!-- History Grade Input -->
+        <div class="form-group col-2">
+            
+            <label for="history" class="float-left font-weight-bold">History</label>
+            
+            <select id="history" class="form-control">
+
+                <option selected>-</option>
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+                <option>D</option>
+                <option>F</option>
+                
+            </select>
+
+        </div>
+
+        <!-- Science Grade Input -->
+        <div class="form-group col-2">
+            
+            <label for="science" class="float-left font-weight-bold">Science</label>
+            
+            <select id="science" class="form-control">
+
+                <option selected>-</option>
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+                <option>D</option>
+                <option>F</option>
+                
+            </select>
+
+        </div>
+
+        <!-- English Grade Input -->
+        <div class="form-group col-2">
+
+            <label for="english" class="float-left font-weight-bold">English</label>
+            
+            <select id="english" class="form-control">
+
+                <option selected>-</option>
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+                <option>D</option>
+                <option>F</option>
+                
+            </select>
+
+        </div>
+
+        <!-- Submit Button -->
+        <div class="form-group form-control-sm col-2">
+            
+            <label for="submit" class="float-left font-weight-bold">*</label>
+
+            <button 
+                type="submit" 
+                class="btn btn-primary btn-block"
+                :disabled="submitStatus"
+                @click="submitMessage"
+            >
+
+                Add Student
+
+            </button>
+
+        </div>
+
+    </form>
 
   </div>
 
