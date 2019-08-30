56b251f57d91342eefd12697031f01f1805cc192
dstearle <dallas.st.earle@gmail.com>
2019-08-30 01:29:50 -0700

    Created file for StudentInput

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <student-input></student-input>
+
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import StudentInput from './components/StudentInput.vue'
 
 export default {
+
   name: 'app',
+
   components: {
-    HelloWorld
+
+    StudentInput
+
   }
+
 }
+
 </script>
 
 <style>
---
+++ src/components/StudentInput.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div id="container">
+
+      <div class="row">
+
+          <div class="col-2">Name</div>
+
+          <div class="col-2">Math</div>
+
+          <div class="col-2">History</div>
+
+          <div class="col-2">Science</div>
+
+          <div class="col-2">English</div>
+
+          <div class="col-2">Submit</div>
+
+      </div>
+
+  </div>
+
+</template>
